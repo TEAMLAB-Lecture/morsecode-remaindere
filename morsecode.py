cec14f443ef874a476bb2d7566e8ae3c6e750d39
remaindere <remaindere@github.com>
2021-01-21 12:40:52 +0900

    upload my code

--- morsecode.py
+++ morsecode.py
@@ -265,7 +265,7 @@ def decoding_sentence(morse_sentence):
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
 
     pattern = re.compile(r'\s\s+')
-    morse_sentence = re.sub(pattern, ' ', morse_sentence)
+    morse_sentence = re.sub(pattern, '  ', morse_sentence)
 
     morse_sentence = morse_sentence.replace("  ","B")
     morse_word_list = morse_sentence.split("B")
